cd71cba4123e3ae6e3a23a7eff17c9942e1426ad
Ms330 <193727568+Mahoshojo-330@users.noreply.github.com>
2026-04-04 19:54:26 +0200

    modified:   Docs/Human/notes.md 	modified:   Engine/src/loaders/scene_parser.c

--- Engine/src/loaders/scene_parser.c
+++ Engine/src/loaders/scene_parser.c
@@ -1,14 +1,13 @@
 /*
-
-1. read the binary file into memory
-2. write into ECS arr
+1. Read the binary file into arena
+2. Write into ECS arrs
 
 Binary format (all little-endian, matches PSP MIPS LE):
-    [uint32  entity_count]
-    [uint32  masks[entity_count]]
-    [Transform_Component[entity_count]]   16 bytes each: float x, float y, int width, int height
-    [Sprite_Component[entity_count]]       8 bytes each: int global_texture_id, uint32 colour_tint
-    [Collider_Component[entity_count]]    20 bytes each: float offset_x, float offset_y, float width, float height, uint32 flags
+    [uint32  entity_count]                  4
+    [uint32  masks[entity_count]]           4 * n
+    [Transform_Component[entity_count]]     16 * n  bytes : float x, float y, int width, int height
+    [Sprite_Component[entity_count]]        8 * n   bytes : int global_texture_id, uint32 colour_tint
+    [Collider_Component[entity_count]]      20 * n  bytes : float offset_x, float offset_y, float width, float height, uint32 flags
 
 Component bit assignments (must match Engine/src/core/ecs.h):
     COMP_ACTIVE    = 1 << 0
@@ -20,3 +19,35 @@ Component bit assignments (must match Engine/src/core/ecs.h):
 
 */
 
+#include "pspiofilemgr.h"
+#include "../core/memory.h"
+
+
+/*
+I have no clue whether this is correct
+*/
+void load_scene(Arena* arena){
+    // read into arena
+    int fd = sceIoOpen("path to file", O_RDONLY, 0777); // is the datatype correct? also the path to file, need to change the file name each time? How? 
+    unsigned char* bytes = (arena -> buffer) + (arena -> offset);
+    int bytes_read = sceIoRead(fd, bytes, 100); 
+
+    (arena -> offset) = Arena_Alloc(arena, bytes_read);
+}
+
+
+/*
+parse the data
+*/
+void parse_scene(Arena* arena, unsigned char* bytes){
+    int n = *(int *)&bytes[0];
+
+    // write masks
+    memcpy(component_masks, bytes[4], n * sizeof(uint32_t));
+
+    // write into component lists
+    memcpy(transforms, bytes[(4 * n) + 4], 5 * sizeof(Transform_Component));
+    memcpy(sprites, bytes[(20 * n) + 4], 5 * sizeof(Sprite_Component));
+    memcpy(collider, bytes[(28 * n) + 4], 5 * sizeof(Collider_Component));
+
+}
